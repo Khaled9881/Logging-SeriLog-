b63e4a496038e62f14272dfad87033357be1bc84
khaled Ibrahiem <khaled.ibrahiem@msa.edu.eg>
2026-07-17 19:54:07 +0300

    Log in

--- Revision of Data Seeding/Controllers/UserController.cs
+++ Revision of Data Seeding/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Revision_of_Data_Seeding.DTOs;
 using Revision_of_Data_Seeding.Identity;
+using Revision_of_Data_Seeding.Models;
 
 namespace Revision_of_Data_Seeding.Controllers
 {
@@ -11,10 +12,14 @@ namespace Revision_of_Data_Seeding.Controllers
     public class UserController : ControllerBase
     {
         private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly PesonsDbContext pesonsDbContext;
 
-        public UserController(UserManager<ApplicationUser> userManager)
+        public UserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, PesonsDbContext pesonsDbContext)
         {
             this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.pesonsDbContext = pesonsDbContext;
         }
 
 
@@ -33,10 +38,29 @@ namespace Revision_of_Data_Seeding.Controllers
 
             if (identityResult.Succeeded)
             {
+                await signInManager.SignInAsync(user, false);
                 return Created();
             }
 
             return BadRequest(identityResult.Errors);
+        }
+
+
+        [HttpPost("signin")]
+        public async Task<IActionResult> Signin(SigninDTO signinDTO)
+        {
+
+            //var user = await userManager.FindByEmailAsync(signinDTO.username);
+            //if (user == null) 
+            //    return Unauthorized();
+
+
+            var result = await signInManager.PasswordSignInAsync(signinDTO.username, signinDTO.password, isPersistent: false, lockoutOnFailure: false);
+
+            if (!result.Succeeded)
+                return Unauthorized();
+
+            return Ok();
         }
     }
 }
---
+++ Revision of Data Seeding/DTOs/SigninDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Revision_of_Data_Seeding.DTOs
+{
+    public class SigninDTO
+    {
+        [Required]
+        public string username { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required]
+        public string password { get; set; }
+    }
+}
